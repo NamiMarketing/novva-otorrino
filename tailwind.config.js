b6118af08f5544436c79946e55a02f0dec8fb118
FeruYasu <fernando_yasumoto@hotmail.com>
2024-06-28 12:44:00 -0300

    chore: update on site

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,9 @@ module.exports = {
         'bege2': '#C1BBB3',
         'bege3': '#7A7169',
         'blue-theme': '#193259',
-        'text': '#4B3F44',
-        'cinza': '#E3DED8'
+        's': '#4B3F44',
+        'cinza': '#E3DED8',
+        'footer': '#979699'
       }
     },
     fontFamily: {
